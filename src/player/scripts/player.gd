d94fbb16d7af3092f6a0e81308a23662ffc8524a
connor-devuk <connorgoodallbusiness@gmail.com>
2024-10-13 15:56:22 +0100

    read me i dare u

--- src/player/scripts/player.gd
+++ src/player/scripts/player.gd
@@ -4,8 +4,13 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var head := $head
+@onready var camera := $head/Camera3D;
+
 
 func _physics_process(delta: float) -> void:
+	$head/SubViewportContainer/SubViewport/view_model_camera.global_transform = camera.global_transform
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -29,7 +34,6 @@ func _physics_process(delta: float) -> void:
 
 
 const MOUSE_SENSITIVITY = 0.0008;
-@onready var camera := $Camera3D;
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -38,3 +42,4 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$head/SubViewportContainer/SubViewport.size = DisplayServer.window_get_size()

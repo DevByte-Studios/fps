d0a2c50ad0a3cf70b591b34414e975fc74d73582
connor-devuk <connorgoodallbusiness@gmail.com>
2024-10-13 16:14:51 +0100

    moved gun stuff to seperate file

---
+++ src/player/gun_manager.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@onready var head := %head
+@onready var camera := %head/Camera3D;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	%head/SubViewportContainer/SubViewport.size = DisplayServer.window_get_size()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	%head/SubViewportContainer/SubViewport/view_model_camera.global_transform = camera.global_transform
--- src/player/scripts/player.gd
+++ src/player/scripts/player.gd
@@ -7,10 +7,7 @@ const JUMP_VELOCITY = 4.5
 @onready var head := $head
 @onready var camera := $head/Camera3D;
 
-
 func _physics_process(delta: float) -> void:
-	$head/SubViewportContainer/SubViewport/view_model_camera.global_transform = camera.global_transform
-
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -42,4 +39,3 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	$head/SubViewportContainer/SubViewport.size = DisplayServer.window_get_size()

e148c6a8bdcc3189642086ebdecf6bbbda297862
connor-devuk <connorgoodallbusiness@gmail.com>
2024-10-17 21:19:54 +0100

    Add weapon config

---
+++ src/weapons/weapon_config.gd
@@ -0,0 +1,14 @@
+class_name WeaponConfig
+extends Resource
+
+@export var display_name: String = "Weapon"
+@export var model_name: String;
+@export var view_model_name: String;
+@export var magazine_size: int;
+@export var reserve_ammo: int;
+@export var has_crosshair: bool;
+
+@export var base_damage: int;
+@export var armor_penetration: int;
+
+

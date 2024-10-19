5b49ee81332146e0788e198a122955dcd55a24a8
P3ntest <juliusvanvoorden@outlook.com>
2024-10-19 14:15:42 +0200

    lots

--- src/components/bullet_hitbox.gd
+++ src/components/bullet_hitbox.gd
@@ -2,10 +2,13 @@ class_name BulletHitbox
 extends Area3D
 
 func _on_bullet_hit(damage: int) -> void:
-	_on_bullet_hit_rpc.rpc(damage)
+	_on_bullet_hit_rpc.rpc(damage * damage_multiplier)
 
 
 @export var health_component: HealthComponent
+
+@export var damage_multiplier: float = 1.0
+@export var is_headshot: bool = false
 
 @rpc("any_peer", "call_local")
 func _on_bullet_hit_rpc(damage: int) -> void:
